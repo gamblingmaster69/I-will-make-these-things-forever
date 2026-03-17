5d62681b2e182f237ec582580fadc7d29cd73492
gamblingmaster69 <marks14q@gmail.com>
2026-03-17 05:11:10 +0200

    Initial commit

---
+++ header.h
@@ -0,0 +1,32 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+
+#define intmegermegermegermegerdetengerper int
+#define maintain main
+#define charmacterpeledacterdetengerper char
+
+#define pregnant_foster_baby_eating_beans_on_friday printf
+
+#define S (
+#define D )
+
+#define s {
+#define d }
+
+#define lessening_port_inhalation <
+#define moreningling_donation >
+
+#define four " four "
+#define fifteen 15
+
+#define is_equal_to =
+#define lend ;
+#define adding_to_padding_the_function_of_our_society_in_the_total_ways_of_a_wave_of_vases_inhalation_PLUS_adding_to_padding_the_function_of_our_society_in_the_total_ways_of_a_wave_of_vases_inhalation ++
+
+#define for_nation for
+#define returnation return
+#define zero 0
+
+#endif // HEADER_H
---
+++ main.c
@@ -0,0 +1,11 @@
+#include "header.h"
+
+intmegermegermegermegerdetengerper maintain S D s
+	charmacterpeledacterdetengerper hi is_equal_to four lend
+	
+	for_nation S intmegermegermegermegerdetengerper i is_equal_to zero lend i lessening_port_inhalation fifteen lend i adding_to_padding_the_function_of_our_society_in_the_total_ways_of_a_wave_of_vases_inhalation_PLUS_adding_to_padding_the_function_of_our_society_in_the_total_ways_of_a_wave_of_vases_inhalation D s
+		pregnant_foster_baby_eating_beans_on_friday S "%d\n", hi  D lend
+	d
+
+	returnation zero lend
+d
